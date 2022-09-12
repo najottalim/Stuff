0eb21dc81f226df4d4d778b275703f3c43724be2
Пользователь <kamron25bek25@gmail.com>
2022-09-12 08:23:38 +0500

    Updated

---
+++ Stuff.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Stuff.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public int Age { get; set; }
+    public string Login { get; set; } = String.Empty;
+    public string MyProperty { get; set; } = String.Empty;
+}
